f870e0c69cf914543b9ffacf81e994a9c9610550
seoung342 <seoung342@gmail.com>
2024-07-09 11:43:45 +0900

    'first'

--- eclipse/JAVA0709/src/exam/Ex02.java
+++ eclipse/JAVA0709/src/exam/Ex02.java
@@ -10,18 +10,19 @@ public class Ex02 {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		int apple;
-		System.out.print("사과의 개수 : ");
-		apple = scan.nextInt();
-		
-		int human;
-		System.out.print("몇명의 사람 : ");
-		human = scan.nextInt();
-		
+		int apple = getCount("사과의 개수 : ", scan);
+		int human = getCount("몇명의 사람 : ", scan);
+	
 		int eat = apple / human;
 		int count = apple % human;
 		
 		System.out.printf("총 %d개의 사과를 %d명의 사람이 %d개씩 먹어서 %d개가 남았다.", apple, human, eat, count);
+	}
+	
+	public static int getCount(String msg, Scanner sc) {
+		System.out.print(msg);
+		String input = sc.nextLine();
+		return Integer.parseInt(input);
 	}
 
 }
