2b1b4b493d6bafcbd8a27c50d9d86ffa0b875445
seoung342 <seoung342@gmail.com>
2024-07-11 13:58:25 +0900

    2024-07-11 13:58

--- eclipse/Practice/src/arrayuser/user.java
+++ eclipse/Practice/src/arrayuser/user.java
@@ -9,9 +9,12 @@ public class user {
 		boolean run1 = true;
 		boolean run2 = true;
 		
-		String[] Id = new String[10];
-		String[] Pass = new String[10];
-		int[] Money = new int[10];
+		String[] Id = new String[10];	// 아이디
+		String[] Pass = new String[10];	// 비밀번호
+		int[] Money = new int[10];	// 잔고
+		String[] Name = new String[10]; // 이름
+		String[] Personal = new String[10];	// 주민번호 핲 6자리
+		String[] Phone = new String[10];	// 전화 번호
 		
 		int index = 0; // 배열 위치
 		int balance = 0; // 입, 출금액
@@ -28,6 +31,7 @@ public class user {
 			num = Integer.parseInt(scan.nextLine());
 			switch(num) {
 			case 1:
+				System.out.println("로그인 처리");
 				System.out.print("아이디 > ");
 				myId = scan.nextLine();
 				System.out.print("비밀번호 > ");
@@ -61,13 +65,16 @@ public class user {
 							System.out.printf("잔고 > %d\n",Money[index]);
 							break;
 						case 4:
+							System.out.println("예금 / 출금 프로그램 종료");
 							run2 = false;
 							break;
 						}
 					}
-				}else {
-					System.out.println("로그인 실패");
+				}else if(Id[index] != null && !Id[index].equals(myId)){
+					System.out.println("로그인 실패 아이디를 다시 확인하세요.");
 					break;
+				}else {
+					System.out.println("로그인 실패 비밀번호를 다시 확인하세요.");
 				}
 				run2 = true;
 				break;
@@ -80,11 +87,19 @@ public class user {
 						break;
 					}
 				}
-				System.out.print("새로운 아이디 > ");
+				System.out.print("회원 가입 \n [필수 정보 입력] \n");
+				System.out.print("1. 이름 : ");
+				Name[index] = scan.nextLine();
+				System.out.print("2. 주민번호 앞 6자리 : ");
+				Personal[index] = scan.nextLine();
+				System.out.print("3. 전화 번호 : ");
+				Phone[index] = scan.nextLine();
+				System.out.print("4. 아이디 : ");
 				Id[index] = scan.nextLine();
-				System.out.print("새로운 비밀번호 > ");
+				System.out.print("5. 비밀번호 ");
 				Pass[index] = scan.nextLine();
-				System.out.println("회원 가입 성공!!");
+				System.out.print("\n [입력된 내용] \n 1. 이름 : "+Name[index]+"\n 2. 주민번호 앞 6자리 : "+Personal[index]
+									+"\n 3. 전화번호 : "+Phone[index]+"\n 4. 아이디 : "+Id[index]+"\n 5.비밀번호 : "+Pass[index]+"\n");
 				break;			
 			case 3:
 				run1 = false;
