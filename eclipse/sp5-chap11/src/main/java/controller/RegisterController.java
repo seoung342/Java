8a4e49a39a2dfa9e219aa2aea6af9c40be3b42bd
seoung342 <seoung342@gmail.com>
2024-10-28 16:48:59 +0900

    2024-10-28 16:50

--- eclipse/sp5-chap11/src/main/java/controller/RegisterController.java
+++ eclipse/sp5-chap11/src/main/java/controller/RegisterController.java
@@ -1,12 +1,18 @@
 package controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import config.RegisterRequestValidator;
 import spring.DuplicateMemberException;
 import spring.MemberRegisterService;
 import spring.RegisterRequest;
@@ -48,13 +54,21 @@ public class RegisterController {
 	}
 
 	@PostMapping("/register/step3")
-	public String handleStep3(RegisterRequest regReq) {
+	public String handleStep3(@Valid RegisterRequest regReq, Errors errors) {
+		if(errors.hasErrors())
+			return "register/step2";
 		try {
 			memberRegisterService.regist(regReq);
 			return "register/step3";
 		} catch (DuplicateMemberException ex) {
+			errors.rejectValue("email", "duplicate");
 			return "register/step2";
 		}
+	}
+	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		binder.setValidator(new RegisterRequestValidator());
 	}
 
 }
