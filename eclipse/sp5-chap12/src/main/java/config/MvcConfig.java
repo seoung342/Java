0bbd332d5570667c87ff0f7ee7e06a66cf9f7176
seoung342 <seoung342@gmail.com>
2024-10-31 19:48:37 +0900

    2024-10-31 19:50

--- eclipse/sp5-chap12/src/main/java/config/MvcConfig.java
+++ eclipse/sp5-chap12/src/main/java/config/MvcConfig.java
@@ -1,7 +1,5 @@
 package config;
 
-import org.springframework.validation.Validator;
-
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,11 +13,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
-	
-//	@Override
-//	public Validator getValidator() {
-//		return new RegisterRequestValidator();
-//	}
 
 	@Override
 	public void configureDefaultServletHandling(
@@ -36,13 +29,15 @@ public class MvcConfig implements WebMvcConfigurer {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/main").setViewName("main");
 	}
-	
+
 	@Bean
 	public MessageSource messageSource() {
-		ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
-		ms.setBasename("message.label");
+		ResourceBundleMessageSource ms = 
+				new ResourceBundleMessageSource();
+		ms.setBasenames("message.label");
 		ms.setDefaultEncoding("UTF-8");
 		return ms;
 	}
 
+	
 }
--- eclipse/sp5-chap12/src/main/java/controller/RegisterController.java
+++ eclipse/sp5-chap12/src/main/java/controller/RegisterController.java
@@ -1,18 +1,13 @@
 package controller;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import config.RegisterRequestValidator;
 import spring.DuplicateMemberException;
 import spring.MemberRegisterService;
 import spring.RegisterRequest;
@@ -25,11 +20,6 @@ public class RegisterController {
 	public void setMemberRegisterService(
 			MemberRegisterService memberRegisterService) {
 		this.memberRegisterService = memberRegisterService;
-	}
-	
-	@GetMapping("/")
-	public String root() {
-		return "redirect:/register/step1";
 	}
 
 	@RequestMapping("/register/step1")
@@ -54,9 +44,11 @@ public class RegisterController {
 	}
 
 	@PostMapping("/register/step3")
-	public String handleStep3(@Valid RegisterRequest regReq, Errors errors) {
-		if(errors.hasErrors())
+	public String handleStep3(RegisterRequest regReq, Errors errors) {
+		new RegisterRequestValidator().validate(regReq, errors);
+		if (errors.hasErrors())
 			return "register/step2";
+
 		try {
 			memberRegisterService.regist(regReq);
 			return "register/step3";
@@ -65,9 +57,5 @@ public class RegisterController {
 			return "register/step2";
 		}
 	}
-	
-//	@InitBinder
-//	protected void initBinder(WebDataBinder binder) {
-//		binder.setValidator(new RegisterRequestValidator());
-//	}
+
 }
